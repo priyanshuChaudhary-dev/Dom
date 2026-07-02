3b1798b9129464d194e297a151ef7ef8a220cc9c
Priyanshu Chaudhary <priyanshu19825@gmail.com>
2026-07-02 10:20:35 +0530

    Add dynamic list creation functionality

---
+++ 02_dynamic_list_creator/script.js
@@ -0,0 +1,26 @@
+const input = document.getElementById("itemInput");
+const addBtn = document.getElementById("addBtn");
+const list = document.getElementById("list");
+
+
+addBtn.addEventListener("click", "keydown", () => {
+    if (input.value === "") {
+        alert("Enter the item");
+        return;
+    }
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    delBtn.textContent = "Delete";
+    delBtn.classList.add("delete");
+
+
+    li.textContent = input.value;
+
+    delBtn.addEventListener("click", () => {
+        li.remove();
+    })
+    li.appendChild(delBtn)
+    list.appendChild(li);
+
+    input.value = "";
+})
